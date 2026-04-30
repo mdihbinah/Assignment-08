e4151ea032955827a60716fb1261e0f7e0565709
mdihbinah <mdihbinah@gmail.com>
2026-04-30 12:22:07 +0600

    Add Marquee.

--- src/app/(Public)/page.js
+++ src/app/(Public)/page.js
@@ -1,10 +1,14 @@
 import Banner from "@/component/Banner";
 import Image from "next/image";
+import Marquee from "react-fast-marquee";
 
 export default function Home() {
   return (
     <div>
       <Banner></Banner>
+      <Marquee className="btn btn-success py-2 font-bold " pauseOnHover={true} speed={100} >
+        New Arrivals: [Tile Name] | Weekly Feature: Modern Geometric Patterns | Join the Community...
+      </Marquee>
     </div>
   )
 }

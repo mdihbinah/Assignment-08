471d213e0a8cf9126c02a63295ce4514ff6ca5a8
mdihbinah <mdihbinah@gmail.com>
2026-05-01 16:28:25 +0600

    Added loading features.

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+
+
+const Loader = () => {
+    return (
+        <div className='w-full h-[10vh]'>
+            <span className='loading loading-spinner loading-xl text-success' ></span>
+        </div>
+    );
+};
+
+export default Loader;
